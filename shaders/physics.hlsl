a06c7229934106f67fc239a051a9a9f3cfb0d9df
Ivan P. <46162167+Krimzo@users.noreply.github.com>
2026-08-13 23:26:14 +0200

    add bad gpu physics

--- shaders/physics.hlsl
+++ shaders/physics.hlsl
@@ -10,6 +10,10 @@ static const uint4 MATERIAL_WATER = create_material(3, 71, 112);
 
 RWTexture2D<uint4> FRAME : register(u0);
 
+bool in_bounds(int2 pos);
+void update_sand_at(int2 pos);
+void update_water_at(int2 pos);
+
 [numthreads(32, 32, 1)]
 void c_shader(uint3 thread_id : SV_DispatchThreadID)
 {
@@ -20,5 +24,67 @@ void c_shader(uint3 thread_id : SV_DispatchThreadID)
         || thread_id.y >= frame_size.y)
         return;
 
-    // ...
+    if (all(FRAME[thread_id.xy] == MATERIAL_SAND))
+        update_sand_at(thread_id.xy);
+    else if (all(FRAME[thread_id.xy] == MATERIAL_WATER))
+        update_water_at(thread_id.xy);
+}
+
+bool in_bounds(int2 pos)
+{
+    int2 frame_size = 0;
+    FRAME.GetDimensions(frame_size.x, frame_size.y);
+    return pos.x >= 0 && pos.x < frame_size.x
+        && pos.y >= 0 && pos.y < frame_size.y;
+}
+
+void update_sand_at(int2 pos)
+{
+    static const int2 OFFSETS[3] = {
+        int2(0, 1),
+        int2(-1, 1),
+        int2(1, 1) };
+
+    [unroll]
+    for (int i = 0; i < 3; i++)
+    {
+        const int2 new_pos = pos + OFFSETS[i];
+        if (!in_bounds(new_pos))
+            continue;
+
+        if (all(FRAME[new_pos] != MATERIAL_AIR)
+            && all(FRAME[new_pos] != MATERIAL_WATER))
+            continue;
+
+        const uint4 temp = FRAME[new_pos];
+        FRAME[new_pos] = FRAME[pos];
+        FRAME[pos] = temp;
+        break;
+    }
+}
+
+void update_water_at(int2 pos)
+{
+    static const int2 OFFSETS[5] = {
+        int2(0, 1),
+        int2(-1, 1),
+        int2(1, 1),
+        int2(-1, 0),
+        int2(1, 0) };
+
+    [unroll]
+    for (int i = 0; i < 5; i++)
+    {
+        const int2 new_pos = pos + OFFSETS[i];
+        if (!in_bounds(new_pos))
+            continue;
+
+        if (all(FRAME[new_pos] != MATERIAL_AIR))
+            continue;
+
+        const uint4 temp = FRAME[new_pos];
+        FRAME[new_pos] = FRAME[pos];
+        FRAME[pos] = temp;
+        break;
+    }
 }
--- source/simulator.cpp
+++ source/simulator.cpp
@@ -137,8 +137,8 @@ void Simulator::handle_render()
 {
     gpu.clear_internal( kl::colors::RED );
     copy_reformat_frame();
-    gpu.copy_resource( gpu.back_target_texture(), m_copy_frame_texture.texture );
     //m_render_frame.draw_circle( window.mouse.position(), brush_radius, selected_material );
+    gpu.copy_resource( gpu.back_target_texture(), m_copy_frame_texture.texture );
     gpu.swap_buffers( true );
     window.set_title( kl::format( "FPS: ", int( 1.0f / timer.delta() ), " Brush Radius: ", brush_radius ) );
 }
